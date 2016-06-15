a366426d9dd6fb3c607d16824c78d039db8fb0f9
huiwq1990 <huiwq1990@163.com>
2016-06-15 23:01:06 +0800

    ese core

---
+++ zhiyin-searcher/ese-core/src/main/java/com/zhiyin/frame/api/ApiServiceApplication.java
@@ -0,0 +1,26 @@
+package com.zhiyin.frame.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+@EnableEurekaClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+@EnableFeignClients
+public class ApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(ApiServiceApplication.class);
+        springApplication.addListeners(new ApplicationPidFileWriter());
+        springApplication.run(args);
+    }
+
+}
