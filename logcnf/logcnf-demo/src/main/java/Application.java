cc0341aed54d0a27bb24465d38f5138faa9a4e06
huiwq1990 <huiwq1990@163.com>
2016-06-18 16:16:09 +0800

    logcnf

--- logcnf/logcnf-demo/src/main/java/Application.java
+++ logcnf/logcnf-demo/src/main/java/Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
-@EnableAutoConfiguration
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
