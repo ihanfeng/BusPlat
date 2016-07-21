ae86ba6be746a99192583efdcc9391e429c7b630
huiwq1990 <huiwq1990@163.com>
2016-07-22 00:00:32 +0800

    new

--- zy-ad/ad-provider/src/main/java/com/zhiyin/ad/config/DubboConfig.java
+++ zy-ad/ad-provider/src/main/java/com/zhiyin/ad/config/DubboConfig.java
@@ -1,12 +1,13 @@
-//package com.zhiyin.ad.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.ImportResource;
-//import org.springframework.context.annotation.Profile;
-//
-//@Profile("product")
-//@Configuration
-//@ImportResource("classpath:context/applicationContext-dubbo.xml")
-//public class DubboConfig {
-//
-//}
+package com.zhiyin.ad.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:config/dubbo-config.properties")
+@ImportResource("classpath:applicationContext-dubbo.xml")
+public class DubboConfig {
+
+}
--- zy-ad/ad-provider/src/main/java/com/zhiyin/ad/service/IAdContentConvService.java
+++
@@ -1,7 +0,0 @@
-package com.zhiyin.ad.service;
-
-/**
- * Created by hg on 2016/7/19.
- */
-public interface IAdContentConvService {
-}
