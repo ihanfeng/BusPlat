6706f983cc5edec2c3ed878fc45ded3f9b2d76b5
huiwq1990 <huiwq1990@163.com>
2016-07-29 15:45:20 +0800

    tmp

---
+++ zy-chat/demo-consumer/src/main/java/com/zhiyin/ourchat/consumer/ConsumerApplication.java
@@ -0,0 +1,23 @@
+package com.zhiyin.ourchat.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"com.zhiyin.ourchat.consumer"})
+public class ConsumerApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringApplication.class);
+    }
+
+}
---
+++ zy-chat/demo-consumer/src/main/java/com/zhiyin/ourchat/consumer/domain/GetUserC2s.java
@@ -0,0 +1,7 @@
+package com.zhiyin.ourchat.consumer.domain;
+
+/**
+ * Created by wangqinghui on 2016/5/18.
+ */
+public class GetUserC2s {
+}
