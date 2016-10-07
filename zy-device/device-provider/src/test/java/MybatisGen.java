222bcb0e971ed8940ac9627bdbed44ee50a77df2
huiwq1990 <huiwq1990@163.com>
2016-10-07 12:01:16 +0800

    new

--- zy-device/device-provider/src/test/java/MybatisGen.java
+++ zy-device/device-provider/src/test/java/MybatisGen.java
@@ -16,7 +16,7 @@ public class MybatisGen {
         try {
             List<String> warnings = new ArrayList<String>();
             boolean overwrite = false;
-            File configFile = new File(MybatisGen.class.getResource("/").getPath() + "generatorConfig-ourchat.xml");
+            File configFile = new File(MybatisGen.class.getResource("/").getPath() + "gen/generatorConfig-device.xml");
             ConfigurationParser cp = new ConfigurationParser(warnings);
             Configuration config = cp.parseConfiguration(configFile);
             DefaultShellCallback callback = new DefaultShellCallback(overwrite);
--- zy-device/doc/zy_device_fix_info.sql
+++ zy-device/doc/zy_device_fix_info.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50621
+Source Server         : 127.0.0.1
+Source Server Version : 50525
 Source Host           : localhost:3306
-Source Database       : test
+Source Database       : zhiyin
 
 Target Server Type    : MYSQL
-Target Server Version : 50621
+Target Server Version : 50525
 File Encoding         : 65001
 
-Date: 2016-09-30 11:10:34
+Date: 2016-10-07 11:55:52
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,38 +20,38 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `zy_device_fix_info`;
 CREATE TABLE `zy_device_fix_info` (
-  `id` bigint(20) NOT NULL COMMENT '设备固定信息',
+  `id` bigint(20) NOT NULL COMMENT '序号',
   `serialno` varchar(1000) DEFAULT NULL,
   `imei` varchar(1000) DEFAULT NULL,
   `uuid` varchar(1000) DEFAULT NULL COMMENT '设备唯一标识符',
-  `model` varchar(1000) DEFAULT NULL,
+  `model` varchar(1000) DEFAULT NULL COMMENT '设备的型号, (iPhone 6,iPad 2 (32nm) 等等)',
   `idfa` varchar(1000) DEFAULT NULL COMMENT '广告位标识符idfa',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='设备固定信息，一般不会更改，用于确定设备唯一性。';
 
 -- ----------------------------
 -- Table structure for zy_device_mov_info
 -- ----------------------------
 DROP TABLE IF EXISTS `zy_device_mov_info`;
 CREATE TABLE `zy_device_mov_info` (
-  `id` bigint(20) NOT NULL COMMENT '设备移动信息',
+  `id` bigint(20) NOT NULL,
   `device_id` bigint(20) DEFAULT NULL COMMENT '设备编号',
   `loc_lat` varchar(255) DEFAULT NULL COMMENT '纬度',
   `loc_lon` varchar(1000) DEFAULT NULL COMMENT '经度',
   `loc_coord` varchar(100) DEFAULT NULL COMMENT '坐标系',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='设备移动信息，设备会接入不同网络，不断改变位置。';
 
 -- ----------------------------
 -- Table structure for zy_device_var_info
 -- ----------------------------
 DROP TABLE IF EXISTS `zy_device_var_info`;
 CREATE TABLE `zy_device_var_info` (
-  `id` bigint(20) NOT NULL COMMENT '设备可变信息',
+  `id` bigint(20) NOT NULL,
   `system_name` varchar(1000) DEFAULT NULL COMMENT '系统名称',
   `system_version` varchar(1000) DEFAULT NULL COMMENT '系统版本号',
   `app_name` varchar(255) DEFAULT NULL COMMENT 'App的名称',
   `app_version` varchar(1000) DEFAULT NULL COMMENT 'App的版本号',
   `app_build_version` varchar(1000) DEFAULT NULL COMMENT 'App build版本号',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='设备可变信息，比如系统版本会随着系统升级改变';
